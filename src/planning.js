bc0c506b7318ad30444863ece0a65f30d65e275d
Menaka Mohanakumar <84263728+menakamohan1999@users.noreply.github.com>
2022-12-24 16:24:07 +0530

    Add files via upload

---
+++ src/planning.js
@@ -0,0 +1,21 @@
+//Book my show planning
+
+//need a reusable carousel
+//Hero carousel
+//Common carousel
+//cast and crew small carosel
+
+//Pages
+//Home page
+//Movie page
+//Categories page
+//Auth page
+//Razorpay payment page
+
+//plan
+//layouts
+//Routes
+//pages
+//components
+
+
